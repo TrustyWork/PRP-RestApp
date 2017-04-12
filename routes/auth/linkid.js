724499f64a69346d99ec47548774a4faa17a1b33
Avdika <avdika@ukr.net>
2017-04-12 21:12:13 +0300

    new file

--- routes/auth/linkid.js
+++ routes/auth/linkid.js
@@ -1,3 +1,15 @@
 /**
  * Created by Avd on 10.04.2017.
  */
+
+passport.use(new LinkedInStrategy({
+        consumerKey: LINKEDIN_API_KEY,
+        consumerSecret: LINKEDIN_SECRET_KEY,
+        callbackURL: "http://127.0.0.1:3000/auth/linkedin/callback"
+    },
+    function(token, tokenSecret, profile, done) {
+        User.findOrCreate({ linkedinId: profile.id }, function (err, user) {
+            return done(err, user);
+        });
+    }
+));
