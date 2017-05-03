89222a270a50022644c8797dde885e789d76857b
Roman Vovk <vovkroman@gmail.com>
2017-05-03 15:58:22 +0300

    style cleanup sp->tab

--- routes/auth/fb.js
+++ routes/auth/fb.js
@@ -7,24 +7,24 @@ const FacebookStrategy = require('passport-facebook').Strategy;
 
 
 passport.use(new FacebookStrategy({
-        'clientID': config.get('auth:facebookAuth:clientID'),
-        'clientSecret': config.get('auth:facebookAuth:clientSecret'),
-        'callbackURL': config.get('rootURL') + ':' + config.get('port') + config.get('auth:facebookAuth:callbackURL')
-    },
-    function(accessToken, refreshToken, profile, done) {
-        profile.username = profile.displayName;
-        userModel.findOrCreate(profile, (err, user) => {
-            if (err) { return done(err); }
-            return done(null, user);
-    });
-    }
+	'clientID': config.get('auth:facebookAuth:clientID'),
+	'clientSecret': config.get('auth:facebookAuth:clientSecret'),
+	'callbackURL': config.get('rootURL') + ':' + config.get('port') + config.get('auth:facebookAuth:callbackURL')
+},
+	function (accessToken, refreshToken, profile, done) {
+		profile.username = profile.displayName;
+		userModel.findOrCreate(profile, (err, user) => {
+			if (err) { return done(err); }
+			return done(null, user);
+		});
+	}
 ));
 
 router.get('/', passport.authenticate('facebook'));
 
-router.get('/callback', passport.authenticate('facebook', { failureRedirect: '/'}),
-    function (req, res) {
-        res.redirect('/users');
-    });
+router.get('/callback', passport.authenticate('facebook', { failureRedirect: '/' }),
+	function (req, res) {
+		res.redirect('/users');
+	});
 
 module.exports = router;
