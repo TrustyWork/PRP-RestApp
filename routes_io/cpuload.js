f6a56ceddca9d793e3dccb2faa7e79ccf6949724
Roman Vovk <vovkroman@gmail.com>
2017-05-10 21:42:25 +0300

    added WS meminfo reporter

--- routes_io/cpuload.js
+++ routes_io/cpuload.js
@@ -1,8 +1,9 @@
 const io = require('wss');
 const loadAVG = require('os').loadavg;
-
+const freemem = require('os').freemem;
 io.on('connection', function (socket) {
 	setInterval(() => {
 		socket.emit('loadavg', loadAVG())
+		socket.emit('freemem', freemem())
 	}, 1000);
 });
