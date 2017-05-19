ee18afdabfd528b90dbc317321a349e44df2dcfb
Roman Vovk <vovkroman@gmail.com>
2017-05-19 14:58:50 +0300

    palnned commit

--- webpack_src/src/app/components/Header/index.jsx
+++ webpack_src/src/app/components/Header/index.jsx
@@ -5,11 +5,6 @@ import FlatButton from 'material-ui/FlatButton'
 import AuthForm from 'app/components/AuthForm';
 
 
-// class Header extends React.Component {
-// handleShow={props.handleAuthFormShow}
-// handleHide={props.handleAuthFormHide}
-// isShown={props.isAuthFormShown}
-// 	render() {
 const Header = ({ isAuthenticated, ...props }) => {
 	console.log('props in Header', props);
 	return (
@@ -24,6 +19,5 @@ const Header = ({ isAuthenticated, ...props }) => {
 		/>
 	)
 }
-//}
 
 export default Header;
