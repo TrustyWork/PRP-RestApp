49d10b8e5eea45c2cf7eb91c1b35a3dab5c73654
Roman Vovk <vovkroman@gmail.com>
2017-06-07 16:54:33 +0300

    fixed WS rooms

--- routes_io/rooms.js
+++ routes_io/rooms.js
@@ -4,13 +4,9 @@ const userModel = require('models/user');
 //Auto join / leave
 
 io.on('connection', (socket) => {
-	if (socket.handshake.session.passport) {
-		userModel.findByUsername(socket.handshake.session.passport.user, (err, user) => {
-			if (err) { return err }
-			if (user) {
-				socket.join(`uid_${user._id.toString()}`);
-			}
-		})
+	if (socket.handshake.session.passport.user) {
+		socket.join(`uid_${socket.handshake.session.passport.user}`);
+		console.log('joining:',`uid_${socket.handshake.session.passport.user}`)
 	}
 
 })
