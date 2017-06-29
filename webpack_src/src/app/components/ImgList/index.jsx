e86d4d233c18e75d0b93c3f3972497e56d3a96d0
Roman Vovk <vovkroman@gmail.com>
2017-06-29 17:56:54 +0300

    added Roboto font as default

--- webpack_src/src/app/components/ImgList/index.jsx
+++ webpack_src/src/app/components/ImgList/index.jsx
@@ -6,27 +6,8 @@ import { connect } from 'react-redux';
 
 import styles from './styles.scss'
 
-
-// const styles = {
-// 	root: {
-// 		display: 'flex',
-// 		flexWrap: 'wrap',
-// 		justifyContent: 'space-around',
-// 	},
-// 	gridList: {
-// 		display: 'flex',
-// 		flexWrap: 'nowrap',
-// 		overflowX: 'auto',
-// 	},
-// 	titleStyle: {
-// 		color: 'rgb(255, 255, 255)',
-// 	},
-// };
-
-
 const ImgList = (props) => {
 
-	//const GridListExampleSingleLine = () => {
 	return (
 		<div className={styles.restolist}>
 			{props.tilesData.map((tile) => (
@@ -41,26 +22,7 @@ const ImgList = (props) => {
 
 		</div>
 	);
-	//		<div className={style.custom}>
-	//			<div style={styles.root}>
-	//				<GridList  style={styles.gridList} cols={2.2}>
-	//					{props.tilesData.map((tile) => (
-	//						<GridTile
-	//							key={tile.idx}
-	//							title={<div>text</div>}
-	//							titleStyle={styles.titleStyle}
-	//							titleBackground="rgba(0,0,0,0.4)"
-	//						>
-	//							<img src={tile.img} />
-	//						</GridTile>
-	//					))}
-	//				</GridList>
-	//			</div>
-	//		</div>
-	//	);
 };
-
-//}
 
 const mapStateToProps = (state) => {
 	return {
